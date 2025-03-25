930f793546723c33ffe6c9c484c662559936de4d
Yinghao Zhu <yhzhu99@gmail.com>
2025-03-25 22:52:15 +0800

    chore: update model zoo for unstructured notes settings

--- src/unstructured_note/utils/config.py
+++ src/unstructured_note/utils/config.py
@@ -4,12 +4,23 @@ MODELS_CONFIG = [
     {"model_type": "BERT", "model_name": "BioBERT", "hf_id": "pritamdeka/BioBert-PubMed200kRCT"},
     {"model_type": "BERT", "model_name": "GatorTron", "hf_id": "UFNLP/gatortron-base"},
     {"model_type": "BERT", "model_name": "Clinical-Longformer", "hf_id": "yikuan8/Clinical-Longformer"},
-    {"model_type": "GPT", "model_name": "GPT-2", "hf_id": "openai-community/gpt2"},
-    {"model_type": "GPT", "model_name": "BioGPT", "hf_id": "microsoft/biogpt"},
+    
+    ### medical LLM
+    # {"model_type": "GPT", "model_name": "BioGPT", "hf_id": "microsoft/biogpt"},
     {"model_type": "GPT", "model_name": "MedAlpaca", "hf_id": "medalpaca/medalpaca-7b"},
-    {"model_type": "GPT", "model_name": "HuatuoGPT", "hf_id": "FreedomIntelligence/HuatuoGPT2-7B"},
+    # {"model_type": "GPT", "model_name": "HuatuoGPT", "hf_id": "FreedomIntelligence/HuatuoGPT2-7B"},
     {"model_type": "GPT", "model_name": "meditron", "hf_id": "epfl-llm/meditron-7b"},
     {"model_type": "GPT", "model_name": "OpenBioLLM", "hf_id": "aaditya/Llama3-OpenBioLLM-8B"},
-    {"model_type": "GPT", "model_name": "Llama3", "hf_id": "meta-llama/Meta-Llama-3-8B-Instruct"},
     {"model_type": "GPT", "model_name": "BioMistral", "hf_id": "BioMistral/BioMistral-7B"},
+    {"model_type": "GPT", "model_name": "Baichuan-M1", "hf_id": "baichuan-inc/Baichuan-M1-14B-Base"},
+    
+    ### general LLM
+    {"model_type": "GPT", "model_name": "GPT-2", "hf_id": "openai-community/gpt2"},
+    {"model_type": "GPT", "model_name": "Llama3", "hf_id": "meta-llama/Llama-3.1-8B"},
+    {"model_type": "GPT", "model_name": "Qwen2.5-7B", "hf_id": "Qwen/Qwen2.5-7B"},
+
+    ### reasoning LLM
+    {"model_type": "GPT", "model_name": "QwQ-32B", "hf_id": "qingcheng-ai/QWQ-32B-FP8"},
+    {"model_type": "GPT", "model_name": "DeepSeek-R1-Distill-Qwen-7B", "hf_id": "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"},
+
 ]
