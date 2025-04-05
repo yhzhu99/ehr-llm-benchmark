ddc7aa563060495bc5c8ce42550f7c54da1e747e
Fieldry <wangzx@stu.pku.edu.cn>
2025-04-05 21:14:33 +0800

    fix: export lab test feature names

--- my_datasets/mimic-iv/preprocess_mimic.py
+++ my_datasets/mimic-iv/preprocess_mimic.py
@@ -87,6 +87,7 @@ pd.to_pickle(test_raw_x, os.path.join(save_dir, "test_raw_x.pkl"))
 pd.to_pickle(test_y, os.path.join(save_dir, "test_y.pkl"))
 pd.to_pickle(test_pid, os.path.join(save_dir, "test_pid.pkl"))
 pd.to_pickle(los_info, os.path.join(save_dir, "los_info.pkl")) # LOS statistics (calculated from the train set)
+pd.to_pickle(labtest_features, os.path.join(save_dir, "labtest_features.pkl")) # All features
 
 # Stratified split dataset into `Training`, `Validation` and `Test` sets
 # Stratified dataset according to `Outcome` column
@@ -161,3 +162,4 @@ pd.to_pickle(test_x, os.path.join(save_dir, "test_x.pkl"))
 pd.to_pickle(test_y, os.path.join(save_dir, "test_y.pkl"))
 pd.to_pickle(test_pid, os.path.join(save_dir, "test_pid.pkl"))
 pd.to_pickle(los_info, os.path.join(save_dir, "los_info.pkl")) # LOS statistics (calculated from the train set)
+pd.to_pickle(labtest_features, os.path.join(save_dir, "labtest_features.pkl")) # All features
