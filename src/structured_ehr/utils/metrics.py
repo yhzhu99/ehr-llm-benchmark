55bd5dfd1f29ada467ea5d2bb6631ff2c7482118
Fieldry <wangzx@stu.pku.edu.cn>
2025-04-09 15:53:50 +0800

    fix: add readmission task

--- src/structured_ehr/utils/metrics.py
+++ src/structured_ehr/utils/metrics.py
@@ -93,7 +93,7 @@ def check_metric_is_better(cur_best, main_metric, score, task):
         if score < cur_best[main_metric]:
             return True
         return False
-    elif task in ["mortality", "multitask"]:
+    elif task in ["mortality", "readmission", "multitask"]:
         if cur_best=={}:
             return True
         if score > cur_best[main_metric]:
