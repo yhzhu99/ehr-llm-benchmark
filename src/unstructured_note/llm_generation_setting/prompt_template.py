75828f53eefc4b4869172f996497fddd166dfd65
Fieldry <wangzx@stu.pku.edu.cn>
2025-04-09 21:32:26 +0800

    chore: update output token limit

--- src/unstructured_note/llm_generation_setting/prompt_template.py
+++ src/unstructured_note/llm_generation_setting/prompt_template.py
@@ -1,6 +1,6 @@
 SYSTEMPROMPT = 'You are an experienced doctor in Intensive Care Unit (ICU) treatment.'
 
 INSTRUCTION_PROMPT = {
-    "outcome": """Based on the intensive care clinical notes, please predict the patient's mortality outcome. 1 for dead, 0 for alive. The closer to 1, the more likely the patient will die. Please output the probability from 0 to 1. Please output your reasoning process and answer in the JSON format: { "reasoning": "your reasoning process", "answer": 0.XX }.""",
-    "readmission": """Based on the intensive care clinical notes, please predict the patient's readmission probability. 1 for readmission, 0 for no readmission. The closer to 1, the more likely the patient will be readmitted. Please output the probability from 0 to 1. Please output your reasoning process and answer in the JSON format: { "reasoning": "your reasoning process", "answer": 0.XX }."""
+    "mortality": """Based on the intensive care clinical notes, please predict the patient's mortality outcome. 1 for dead, 0 for alive. The closer to 1, the more likely the patient will die. Please output the probability from 0 to 1. Please output your thinking process and answer in the JSON format: { "think": "your thinking process", "answer": 0.XX }. Your output should be less than 500 words.""",
+    "readmission": """Based on the intensive care clinical notes, please predict the patient's readmission probability. 1 for readmission, 0 for no readmission. The closer to 1, the more likely the patient will be readmitted. Please output the probability from 0 to 1. Please output your thinking process and answer in the JSON format: { "think": "your thinking process", "answer": 0.XX }. Your output should be less than 500 words.""",
 }
