6f64ef50bcf01a8b50ba6cca898c346f2830ed76
Fieldry <wangzx@stu.pku.edu.cn>
2025-04-10 23:00:29 +0800

    chore: update llm api configs

--- src/structured_ehr/utils/llm_configs.py
+++ src/structured_ehr/utils/llm_configs.py
@@ -4,7 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-LLM_MODELS_SETTINGS = {
+MODELS_CONFIG = {
+    ### medical LLM
+    "OpenBioLLM": {"model_type": "GPT", "model_name": "OpenBioLLM", "lmstudio_id": "openbiollm-llama3-8b"},
+
+    ### general LLM
+    "GPT-2": {"model_type": "GPT", "model_name": "GPT-2", "hf_id": "openai-community/gpt2"},
+    "Qwen2.5-7B": {"model_type": "GPT", "model_name": "Qwen2.5-7B", "lmstudio_id": "qwen2.5-7b-instruct-1m"},
+    "Gemma-3-4B": {"model_type": "GPT", "model_name": "gemma-3-4b-pt", "lmstudio_id": "gemma-3-4b-it"},
+
+    ### reasoning LLM
+    "HuatuoGPT-o1-7B": {"model_type": "GPT", "model_name": "HuatuoGPT-o1-7B", "lmstudio_id": "huatuogpt-o1-7b"},
+    "DeepSeek-R1-7B": {"model_type": "GPT", "model_name": "DeepSeek-R1-Distill-Qwen-7B", "lmstudio_id": "deepseek-r1-distill-qwen-7b"},
+}
+
+LLM_API_CONFIG = {
     "deepseek-v3-official": {
         "api_key": os.getenv("DEEPSEEK_API_KEY"),
         "base_url": "https://api.deepseek.com",
@@ -47,4 +61,8 @@ LLM_MODELS_SETTINGS = {
         "comment": "DeepSeek R1 Reasoning Model Ark",
         "reasoning": True,
     },
+    "llm-studio": {
+        "api_key": os.getenv("LLMSTUDIO_API_KEY"),
+        "base_url": "https://llmapi.yhzhu.uk/v1"
+    }
 }
