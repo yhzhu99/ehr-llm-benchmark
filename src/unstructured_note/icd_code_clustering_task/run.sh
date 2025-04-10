cfbdbfcaf33c3406789e18c6aee760a77fc054c8
Yinghao Zhu <yhzhu99@gmail.com>
2025-04-11 03:24:10 +0800

    chore: Removes redundant model from the script.
    Removes the duplicate "QwQ-32B" model from the list of models used in the get_icd_embeddings.py script.

--- src/unstructured_note/icd_code_clustering_task/run.sh
+++ src/unstructured_note/icd_code_clustering_task/run.sh
@@ -1,4 +1,4 @@
-for model in BERT ClinicalBERT BioBERT GatorTron Clinical-Longformer BioGPT meditron OpenBioLLM BioMistral GPT-2 Qwen2.5-7B gemma-3-4b-pt HuatuoGPT-o1-7B QwQ-32B DeepSeek-R1-Distill-Qwen-7B; do
+for model in BERT ClinicalBERT BioBERT GatorTron Clinical-Longformer BioGPT meditron OpenBioLLM BioMistral GPT-2 Qwen2.5-7B gemma-3-4b-pt HuatuoGPT-o1-7B DeepSeek-R1-Distill-Qwen-7B; do
   python src/unstructured_note/icd_code_clustering_task/get_icd_embeddings.py --model $model
 done
 
