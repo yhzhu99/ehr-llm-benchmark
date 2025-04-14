2fa347ac3aaa61a0ad068d8e9229b8a87f9b23fa
Fieldry <wangzx@stu.pku.edu.cn>
2025-04-14 15:10:48 +0800

    feat: init ml and dl models

--- src/structured_ehr/models/__init__.py
+++ src/structured_ehr/models/__init__.py
@@ -1,2 +1,32 @@
-from .adacare import AdaCare
+# Machine Learning Models
+from .catboost import CatBoost
+from .dt import DT
+from .rf import RF
+from .xgboost import XGBoost
+
+# Deep Learning Models
 from .gru import GRU
+from .lstm import LSTM
+from .transformer import Transformer
+from .rnn import RNN
+
+# Deep Learning Models for structured EHR
+from .adacare import AdaCare
+from .aicare import AiCare
+from .concare import ConCare
+from .grasp import Grasp
+
+__all__ = [
+    "CatBoost",
+    "DT",
+    "RF",
+    "XGBoost",
+    "GRU",
+    "LSTM",
+    "Transformer",
+    "RNN",
+    "AdaCare",
+    "AiCare",
+    "ConCare",
+    "Grasp",
+]
--- src/structured_ehr/models/lstm.py
+++ src/structured_ehr/models/lstm.py
@@ -1,14 +1,14 @@
 from torch import nn
 
 
-class GRU(nn.Module):
+class LSTM(nn.Module):
     def __init__(self, input_dim, hidden_dim, act_layer=nn.GELU, drop=0.0, **kwargs):
         super().__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
 
-        self.gru = nn.GRU(input_size=input_dim, hidden_size=hidden_dim, num_layers=1, batch_first=True)
+        self.lstm = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim, num_layers=1, batch_first=True)
 
     def forward(self, x, **kwargs):
-        y, _ = self.gru(x)[:, -1, :]
+        y, _ = self.lstm(x)[:, -1, :]
         return y
