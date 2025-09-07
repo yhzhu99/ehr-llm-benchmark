b115bdaab344709509247faa5550c960767c3bba
Fieldry <wangzx@stu.pku.edu.cn>
2025-09-07 13:52:27 +0800

    chore: update llm api configs

--- src/structured_ehr/utils/llm_configs.py
+++ src/structured_ehr/utils/llm_configs.py
@@ -19,58 +19,24 @@ MODELS_CONFIG = {
 }
 
 LLM_API_CONFIG = {
-    "deepseek-v3-official": {
+    "deepseek-v3-chat": {
         "api_key": os.getenv("DEEPSEEK_API_KEY"),
-        "base_url": "https://api.deepseek.com",
+        "base_url": "https://api.deepseek.com/v1",
         "model_name": "deepseek-chat",
-        "comment": "DeepSeek V3 Official",
-        "reasoning": False,
+        "comment": "DeepSeek V3 Chat",
     },
-    "deepseek-r1-official": {
+    "deepseek-v3-reasoner": {
         "api_key": os.getenv("DEEPSEEK_API_KEY"),
-        "base_url": "https://api.deepseek.com",
+        "base_url": "https://api.deepseek.com/v1",
         "model_name": "deepseek-reasoner",
-        "comment": "DeepSeek R1 Reasoning Model Official",
-        "reasoning": True,
-    },
-    "deepseek-v3-ali": {
-        "api_key": os.getenv("DASHSCOPE_API_KEY"),
-        "base_url": "https://dashscope.aliyuncs.com/compatible-mode/v1",
-        "model_name": "deepseek-v3",
-        "comment": "DeepSeek V3 Ali",
-        "reasoning": False,
-    },
-    "deepseek-r1-ali": {
-        "api_key": os.getenv("DASHSCOPE_API_KEY"),
-        "base_url": "https://dashscope.aliyuncs.com/compatible-mode/v1",
-        "model_name": "deepseek-r1",
-        "comment": "DeepSeek R1 Reasoning Model Ali",
-        "reasoning": True,
-    },
-    "deepseek-v3-ark": {
-        "api_key": os.getenv("ARK_API_KEY"),
-        "base_url": "https://ark.cn-beijing.volces.com/api/v3",
-        "model_name": "deepseek-v3-250324",
-        "comment": "DeepSeek V3 Ark",
-        "reasoning": False,
-    },
-    "deepseek-r1-ark": {
-        "api_key": os.getenv("ARK_API_KEY"),
-        "base_url": "https://ark.cn-beijing.volces.com/api/v3",
-        "model_name": "deepseek-r1-250120",
-        "comment": "DeepSeek R1 Reasoning Model Ark",
-        "reasoning": True,
+        "comment": "DeepSeek V3 Reasoner",
     },
     "llm-studio": {
         "api_key": os.getenv("LLMSTUDIO_API_KEY"),
         "base_url": "https://llm.yhzhu.uk/v1"
     },
-    "v8": {
-        "api_key": os.getenv("XDAI_V8_API_KEY"),
-        "base_url": "https://xdaicn.top/v1",
-    },
-    "default": {
-        "api_key": os.getenv("XDAI_DEFAULT_API_KEY"),
-        "base_url": "https://xdaicn.top/v1",
-    },
+    "laozhang": {
+        "api_key": os.getenv("LAOZHANG_API_KEY"),
+        "base_url": "https://api.laozhang.ai/v1"
+    }
 }
