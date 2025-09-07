300dbab7334e464598a8a8239f47772fc765273a
Fieldry <wangzx@stu.pku.edu.cn>
2025-09-07 11:42:04 +0800

    chore: update llm api configs

--- src/unstructured_note/utils/config.py
+++ src/unstructured_note/utils/config.py
@@ -52,47 +52,17 @@ LMSTUDIO_MODELS_CONFIG = {
 }
 
 LLM_API_CONFIG = {
-    "deepseek-v3-official": {
+    "deepseek-v3-chat": {
         "api_key": os.getenv("DEEPSEEK_API_KEY"),
-        "base_url": "https://api.deepseek.com",
+        "base_url": "https://api.deepseek.com/v1",
         "model_name": "deepseek-chat",
-        "comment": "DeepSeek V3 Official",
-        "reasoning": False,
+        "comment": "DeepSeek V3 Chat",
     },
-    "deepseek-r1-official": {
+    "deepseek-v3-reasoner": {
         "api_key": os.getenv("DEEPSEEK_API_KEY"),
-        "base_url": "https://api.deepseek.com",
-        "model_name": "deepseek-reasoner",
-        "comment": "DeepSeek R1 Reasoning Model Official",
-        "reasoning": True,
-    },
-    "deepseek-v3-ali": {
-        "api_key": os.getenv("DASHSCOPE_API_KEY"),
-        "base_url": "https://dashscope.aliyuncs.com/compatible-mode/v1",
-        "model_name": "deepseek-v3",
-        "comment": "DeepSeek V3 Ali",
-        "reasoning": False,
-    },
-    "deepseek-r1-ali": {
-        "api_key": os.getenv("DASHSCOPE_API_KEY"),
-        "base_url": "https://dashscope.aliyuncs.com/compatible-mode/v1",
-        "model_name": "deepseek-r1",
-        "comment": "DeepSeek R1 Reasoning Model Ali",
-        "reasoning": True,
-    },
-    "deepseek-v3-ark": {
-        "api_key": os.getenv("ARK_API_KEY"),
-        "base_url": "https://ark.cn-beijing.volces.com/api/v3",
-        "model_name": "deepseek-v3-250324",
-        "comment": "DeepSeek V3 Ark",
-        "reasoning": False,
-    },
-    "deepseek-r1-ark": {
-        "api_key": os.getenv("ARK_API_KEY"),
-        "base_url": "https://ark.cn-beijing.volces.com/api/v3",
-        "model_name": "deepseek-r1-250120",
-        "comment": "DeepSeek R1 Reasoning Model Ark",
-        "reasoning": True,
+        "base_url": "https://api.deepseek.com/v1",
+        "model_name": "deepseek-v3-reasoner",
+        "comment": "DeepSeek V3 Reasoner",
     },
     "llm-studio": {
         "api_key": os.getenv("LLMSTUDIO_API_KEY"),
